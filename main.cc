399b58dcf0e9ff97744e971695021d4f21cf2906
brisamar <brisamarramos@csu.fullerton.edu>
2022-02-12 05:39:37 +0000

    problem 2 done

--- main.cc
+++ main.cc
@@ -3,30 +3,36 @@
 #include <iomanip>
 int main() {
 
-  double mealcost = 0; 
-  float tipPercentage =0 ; 
-  double taxes = 0;
-  double tip=0;
-  double totalcost = 0;
-  
-  
+  double mealcost = 0.00; 
+  double tipPercentage =0.00 ; 
+  double taxes = 0.00;
+  double tip=0.000;
+  double totalcost = 0.00;
+ 
+ 
 
   
   
-  std::cout << "Please input meal cost: \n";
+  std::cout << "Please input meal cost: ";
   std::cin >> mealcost;
+  
   taxes = (7.5/100) * mealcost;
-  std::cout << "Please input tip percentage: \n";
+  std::cout <<"Please input tip percentage: ";
   std::cin >> tipPercentage;
-  tip = (tipPercentage*mealcost);
-  totalcost = (tipPercentage+taxes+mealcost);
+  
+  tip = (tipPercentage/100) * mealcost;
+  
+  totalcost = (tip+taxes+mealcost);
   std::cout << "Restaurant Bill \n";
   std::cout << "====================\n";
-  std::cout << "Subtotal: $ " << mealcost << "\n";
+  std::cout << "Subtotal: $" << std::fixed <<std::setprecision(2)<< mealcost << "\n";
   std::cout << "Taxes: $" << taxes << "\n";
-  std::cout << "Tip: $" << tip << "\n";
+ 
+   
+  std::cout << "Tip: $" << std::fixed << std::setprecision(2)<< tip << "\n";
   std::cout << "====================\n";
   std::cout << "Total: $" << totalcost<<"\n";
+  return 0;
 }  
 
 
